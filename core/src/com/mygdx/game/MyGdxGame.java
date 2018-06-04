c0c61bdfa0287f8fad19124de4b422073b119b27
DanielRabinovitz <danielrabinovitz@gmail.com>
2018-06-04 09:10:15 -0400

    Sprite animation is a pain

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,25 +3,33 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import static com.badlogic.gdx.Gdx.input;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
 	Sprite sprite;
+	TextureAtlas textureAtlas;
+	TextureRegion textureRegion;
+
 
 	@Override
 	public void create () {
+		textureAtlas=new TextureAtlas();
 		batch = new SpriteBatch();
-		img = new Texture("BluePulse.png");
+		img = new Texture("PixelHouse.png");
 		sprite = new Sprite(img);
 		sprite.setPosition(Gdx.graphics.getWidth()/2-sprite.getWidth()/2, Gdx.graphics.getHeight()/2-sprite.getHeight()/2);
 		sprite.setScale(25f);
+		Gdx.input.setInputProcessor(this);
 
 	}
 
@@ -30,6 +38,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		sprite.setScale(SpriteSize);
+		//"Polling" type methods. Runs a check every frame
 		if(input.isKeyPressed(Input.Keys.A)){ sprite.translateX(-3f); sprite.rotate(5f);}
 		if(input.isKeyPressed(Input.Keys.D)){ sprite.translateX(3f); sprite.rotate(-5f);}
 		if(input.isKeyPressed(Input.Keys.W)){ if(SpriteSize <35) SpriteSize +=0.5;}
@@ -53,5 +62,49 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void dispose (){
 		batch.dispose();
 		img.dispose();
+	}
+
+	//"Event" type methods. Only occurs when key is pressed.
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode==Input.Keys.E){sprite.translate(10,10);}
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode==Input.Keys.E){sprite.translate(-10,-10);}
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
